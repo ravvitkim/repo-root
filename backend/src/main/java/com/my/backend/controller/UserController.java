c4da467c50f037bea9dee11ac3e0891dabe3f546
ravvitkim <mirkim6086@naver.com>
2025-09-29 15:41:08 +0900

    1차 로컬테스트 완료

---
+++ backend/src/main/java/com/my/backend/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.my.backend.controller;
+
+import com.my.backend.dto.CreateUserRequest;
+import com.my.backend.entity.User;
+import com.my.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> list() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody CreateUserRequest request) {
+        return service.create(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
